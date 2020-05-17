eb9939a38776f44634ab4ee1473b14a2e215b795
Keegan Leary <keegs@Keegans-iMac.local>
2020-05-16 22:08:19 -0700

    First Commit

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+    public float speed = 10;
+
+    void Update()
+    {
+        if (Input.GetKey(moveUp)) {
+            rb.velocity = new Vector2(0.0f, speed);
+        }
+        else if (Input.GetKey(moveDown)) {
+            rb.velocity = new Vector2(0.0f, -speed);
+        }
+        else {
+            rb.velocity = new Vector2(0.0f, 0.0f);
+        }
+    }
+}
