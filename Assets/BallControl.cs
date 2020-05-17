50bd9e015e9797fd8f8aed7973104b93280aa1d8
Keegan Leary <keegs@Keegans-iMac.local>
2020-05-17 08:41:24 -0700

    Ball

---
+++ Assets/BallControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float randomNumber = Random.Range(0f, 1.0f);
+
+        if (randomNumber < 0.5) {
+            rb.AddForce(new Vector2(80,10));
+        }
+        else {
+            rb.AddForce(new Vector2(-80,-10));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D colInfo) {
+        if (colInfo.collider.tag == "Player") {
+            // Ball speed increases a bit with each hit and is dependent upon 1/3 of paddle speed.
+            rb.velocity = new Vector2(rb.velocity.x * 1.02f, rb.velocity.y/2 + colInfo.rigidbody.velocity.y/3);
+        }
+    }
+}
--- Assets/GameSetup.cs
+++ Assets/GameSetup.cs
@@ -8,10 +8,10 @@ public class GameSetup : MonoBehaviour
     public BoxCollider2D leftWall;
     public BoxCollider2D rightWall;
 
-    public Transform player01;
-    public Transform player02;
+    public Transform Player01;
+    public Transform Player02;
 
-    void Update()
+    void Start()
     {
         topWall.size = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width * 2f, 0f, 0f)).x, 1f);
         topWall.offset = new Vector2(0f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f)).y + 0.5f);
@@ -24,5 +24,10 @@ public class GameSetup : MonoBehaviour
 
         rightWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height * 2f, 0f)).y);
         rightWall.offset = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 0.5f, 0f);
+
+
+        //Player01.position = mainCam.ScreenToWorldPoint(new Vector3(75f, Screen.height*0.5f, 0f));
+        Player01.position = mainCam.ScreenToWorldPoint(new Vector3(75f, Screen.height*0.5f, 10f));
+        Player02.position = mainCam.ScreenToWorldPoint(new Vector3(Screen.width - 75f, Screen.height*0.5f, 10f));
     }
 }
